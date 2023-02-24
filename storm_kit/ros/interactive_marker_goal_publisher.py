4fc4fc54f26aa923a6e6195a262a7874a9e9a8bd
Mohak Bhardwaj <mohak.bhardwaj@gmail.com>
2023-02-23 17:09:25 -0800

    added reacher node

---
+++ storm_kit/ros/interactive_marker_goal_publisher.py
@@ -0,0 +1,220 @@
+from copy import deepcopy
+import numpy as np
+import torch
+
+import rospy
+from geometry_msgs.msg import PoseStamped 
+from sensor_msgs.msg import JointState
+from interactive_markers.interactive_marker_server import *
+from visualization_msgs.msg import *
+
+from storm_kit.differentiable_robot_model import DifferentiableRobotModel
+from storm_kit.differentiable_robot_model.coordinate_transform import matrix_to_quaternion
+
+
+class InteractiveMarkerGoalPub():
+    def __init__(self):
+        self.joint_states_topic = rospy.get_param('~joint_states_topic', 'joint_states')
+        self.ee_goal_topic = rospy.get_param('~ee_goal_topic', 'ee_goal')
+        self.goal_pub_freq = rospy.get_param('~goal_pub_freq', 10)
+        self.fixed_frame = rospy.get_param('~fixed_frame', 'panda_link0')
+        self.robot_urdf = '../../content/assets/urdf/franka_description/franka_panda_no_gripper.urdf'
+        self.ee_frame = rospy.get_param('~ee_frame', 'ee_link')
+        
+
+        #ROS Initialization
+        self.ee_goal = PoseStamped()
+        self.gripper_state = JointState()
+        self.robot_state = JointState()
+
+        self.ee_goal_pub = rospy.Publisher(self.ee_goal_topic, PoseStamped, queue_size=1, tcp_nodelay=True, latch=False)
+        self.state_sub = rospy.Subscriber(self.joint_states_topic, JointState, self.robot_state_callback, queue_size=1)
+
+        #STORM Related
+        self.tensor_args = {'device': 'cpu', 'dtype': torch.float32}
+        self.robot_model = DifferentiableRobotModel(self.robot_urdf, None, 
+                            tensor_args=self.tensor_args)
+            
+        #Buffers
+
+        self.rate = rospy.Rate(self.goal_pub_freq)
+        self.state_received = False
+        while not self.state_received:
+            pass
+        #we set self.ee_goal to the initial robot pose
+        self.update_ee_goal_to_current()
+
+        self.setup_interactive_marker_server()
+
+
+
+
+    def setup_interactive_marker_server(self):
+        # create an interactive marker server on the topic namespace simple_marker
+        self.server = InteractiveMarkerServer("goal_marker")
+
+        # create an interactive marker for our server
+        self.int_marker = InteractiveMarker()
+        self.int_marker.header.frame_id = self.fixed_frame #"panda_link0"
+        self.int_marker.name = "goal_marker"
+        self.int_marker.description = "End-effector Goal"
+        self.int_marker.scale = 0.2
+        # self.int_marker.pose.position = self.curr_goal_ros.pose.position
+        self.int_marker.pose = self.ee_goal.pose
+
+
+        # create a grey box marker
+        self.box_marker = Marker()
+        self.box_marker.type = Marker.CUBE
+        self.box_marker.scale.x = 0.1
+        self.box_marker.scale.y = 0.1
+        self.box_marker.scale.z = 0.1
+        self.box_marker.color.r = 0.0
+        self.box_marker.color.g = 0.5
+        self.box_marker.color.b = 0.5
+        self.box_marker.color.a = 1.0
+
+        # create a non-interactive control which contains the box
+        self.box_control = InteractiveMarkerControl()
+        self.box_control.always_visible = True
+        self.box_control.markers.append(self.box_marker)
+
+        # add the control to the interactive marker
+        self.int_marker.controls.append(self.box_control)
+
+        # create a control which will move the box
+        # this control does not contain any markers,
+        # which will cause RViz to insert two arrows
+        self.move_x_control = InteractiveMarkerControl()
+        self.move_x_control.name = "move_x"
+        self.move_x_control.orientation.w = 1
+        self.move_x_control.orientation.x = 1
+        self.move_x_control.orientation.y = 0
+        self.move_x_control.orientation.z = 0   
+        self.move_x_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
+        # add the control to the interactive marker
+        self.int_marker.controls.append(self.move_x_control)
+
+        self.move_y_control = InteractiveMarkerControl()
+        self.move_y_control.name = "move_y"
+        self.move_y_control.orientation.w = 1
+        self.move_y_control.orientation.x = 0
+        self.move_y_control.orientation.y = 0
+        self.move_y_control.orientation.z = 1                
+        self.move_y_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
+        self.int_marker.controls.append(self.move_y_control)
+
+        self.move_z_control = InteractiveMarkerControl()
+        self.move_z_control.name = "move_z"
+        self.move_z_control.orientation.w = 1
+        self.move_z_control.orientation.x = 0
+        self.move_z_control.orientation.y = 1
+        self.move_z_control.orientation.z = 0
+        self.move_z_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
+
+        self.int_marker.controls.append(self.move_z_control)
+
+        # create controls for rotation
+        self.rotate_x_control = InteractiveMarkerControl()
+        self.rotate_x_control.orientation.w = 1
+        self.rotate_x_control.orientation.x = 1
+        self.rotate_x_control.orientation.y = 0
+        self.rotate_x_control.orientation.z = 0
+        self.rotate_x_control.name = "rotate_x"
+        self.rotate_x_control.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
+        # if fixed:
+            # control.orientation_mode = InteractiveMarkerControl.FIXED
+        self.int_marker.controls.append(self.rotate_x_control)
+
+        self.rotate_y_control = InteractiveMarkerControl()
+        self.rotate_y_control.orientation.w = 1
+        self.rotate_y_control.orientation.x = 0
+        self.rotate_y_control.orientation.y = 1
+        self.rotate_y_control.orientation.z = 0
+        self.rotate_y_control.name = "rotate_y"
+        self.rotate_y_control.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
+        # if fixed:
+            # control.orientation_mode = InteractiveMarkerControl.FIXED
+        self.int_marker.controls.append(self.rotate_y_control)
+
+        self.rotate_z_control = InteractiveMarkerControl()
+        self.rotate_z_control.orientation.w = 1
+        self.rotate_z_control.orientation.x = 0
+        self.rotate_z_control.orientation.y = 0
+        self.rotate_z_control.orientation.z = 1
+        self.rotate_z_control.name = "rotate_z"
+        self.rotate_z_control.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
+        # if fixed:
+            # control.orientation_mode = InteractiveMarkerControl.FIXED
+        self.int_marker.controls.append(self.rotate_z_control)
+
+        # add the interactive marker to our collection &
+        # tell the server to call marker_callback() when feedback arrives for it
+        self.server.insert(self.int_marker, self.marker_callback)
+
+        # 'commit' changes and send to all clients
+        self.server.applyChanges()
+
+    def marker_callback(self, msg):
+        self.ee_goal.header = msg.header
+        self.ee_goal.pose = deepcopy(msg.pose)
+
+    def goal_pub_loop(self):
+        while not rospy.is_shutdown():
+            self.ee_goal_pub.publish(self.ee_goal)
+            self.rate.sleep()
+    
+    def robot_state_callback(self, msg):
+        self.state_received = True
+        # save gripper state
+        self.gripper_state.header = msg.header
+        self.gripper_state.position = msg.position[0:2]
+        self.gripper_state.velocity = msg.velocity[0:2]
+        self.gripper_state.effort = msg.effort[0:2]
+
+        #save robot state
+        self.robot_state.header = msg.header
+        self.robot_state.position = msg.position[2:]
+        self.robot_state.velocity = msg.velocity[2:]
+        self.robot_state.effort = msg.effort[2:]
+
+
+    def update_ee_goal_to_current(self):
+        q_robot = torch.as_tensor(self.robot_state.position, **self.tensor_args).unsqueeze(0)
+        qd_robot = torch.as_tensor(self.robot_state.velocity, **self.tensor_args).unsqueeze(0)
+        # q_gripper = torch.as_tensor(self.gripper_state.position, **self.tensor_args).unsqueeze(0)
+        # qd_gripper = torch.as_tensor(self.gripper_state.velocity, **self.tensor_args).unsqueeze(0)
+        # q = torch.cat((q_robot, q_gripper), dim=-1)
+        # qd = torch.cat((qd_robot, qd_gripper), dim=-1)
+
+
+        curr_ee_pos, curr_ee_rot = self.robot_model.compute_forward_kinematics(
+            q_robot, qd_robot, link_name=self.ee_frame)
+        curr_ee_quat = matrix_to_quaternion(curr_ee_rot)
+        # self.curr_ee_quat = self.curr_ee_quat / torch.norm(self.curr_ee_quat) #normalize quaternion
+
+
+        #convert to pose stamped message
+        self.ee_goal.header.stamp = rospy.Time.now()
+        self.ee_goal.pose.position.x = curr_ee_pos[0][0].item() 
+        self.ee_goal.pose.position.y = curr_ee_pos[0][1].item() 
+        self.ee_goal.pose.position.z = curr_ee_pos[0][2].item() 
+        self.ee_goal.pose.orientation.w = curr_ee_quat[0][0].item() 
+        self.ee_goal.pose.orientation.x = curr_ee_quat[0][1].item() 
+        self.ee_goal.pose.orientation.y = curr_ee_quat[0][2].item() 
+        self.ee_goal.pose.orientation.z = curr_ee_quat[0][3].item()
+    
+    def close(self):
+        self.ee_goal_pub.unregister()
+        self.state_sub.unregister()
+
+if __name__ == "__main__":
+    rospy.init_node("interactive_marker_goal_node", anonymous=True, disable_signals=True)    
+
+    goal_node = InteractiveMarkerGoalPub()
+
+    try:
+        goal_node.goal_pub_loop()
+    except KeyboardInterrupt:
+        print('Exiting')
+        goal_node.close()
--- storm_kit/ros/reacher_node.py
+++ storm_kit/ros/reacher_node.py
@@ -10,11 +10,10 @@ torch.backends.cudnn.allow_tf32 = True
 
 #ROS Imports
 import rospy
-from geometry_msgs.msg import PoseStamped, Point
-from sensor_msgs.msg import JointState, PointCloud2
-from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
-from visualization_msgs.msg import Marker, MarkerArray
-from std_msgs.msg import String, Header
+from geometry_msgs.msg import PoseStamped 
+from sensor_msgs.msg import JointState
+# from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
+# from std_msgs.msg import String, Header
 
 #STORM imports
 from storm_kit.mpc.task.reacher_task import ReacherTask
@@ -25,55 +24,68 @@ class MPCReacherNode():
         self.joint_states_topic = rospy.get_param('~joint_states_topic', 'joint_states')
         self.joint_command_topic = rospy.get_param('~joint_command_topic', 'franka_motion_control/joint_command')
         self.ee_goal_topic = rospy.get_param('~ee_goal_topic', 'ee_goal')
-        self.pointcloud_topic = rospy.get_param('~pointcloud_topic', '/camera/depth/color/points_filtered')
-        self.diagnostics_topic = rospy.get_param('~diagnostics_topic', 'mpc_diagnostics')
-        self.fixed_frame = rospy.get_param('~fixed_frame', 'panda_link0')
-        self.pointcloud_frame = rospy.get_param('~pointcloud_frame', '/camera/depth/color/points_filtered')
-        self.joint_names = rospy.get_param('robot_joint_names', None)
+        # self.joint_names = rospy.get_param('robot_joint_names', None)
         self.world_description = rospy.get_param('world_description', os.path.abspath('../../content/configs/gym/collision_table.yml'))
         self.mpc_config = rospy.get_param('mpc_config', os.path.abspath('../../content/configs/mpc/franka_reacher_real_robot.yml'))
         self.device = torch.device('cuda', 0)
         self.tensor_args = {'device': self.device, 'dtype': torch.float32}
+        self.joint_names = ['panda_joint1', 'panda_joint2', 'panda_joint3', 'panda_joint4', 'panda_joint5', 'panda_joint6', 'panda_joint7']
 
         #STORM Initialization
         self.robot_coll_description = os.path.abspath('../../content/configs/robot/franka_real_robot.yml')
         self.policy = ReacherTask(self.mpc_config, self.robot_coll_description, self.world_description, self.tensor_args)
-
-        #ROS Initialization
-        self.command_pub = rospy.Publisher(self.joint_command_topic, JointState, queue_size=1, tcp_nodelay=True, latch=False)
-        self.state_sub = rospy.Subscriber(self.joint_states_topic, JointState, self.robot_state_callback, queue_size=1)
-        self.ee_goal_sub = rospy.Subscriber(self.ee_goal_topic, PoseStamped, self.ee_goal_callback, queue_size=1)
-        self.control_freq = 50.0
-        self.rate = rospy.Rate(self.control_freq)
+        self.control_dt = 0.02 #TODO: this needs to be read from somewhere
 
         #buffers for different messages
         self.mpc_command = JointState()
         self.mpc_command.name = self.joint_names
         self.mpc_command.effort = np.zeros(7)
-        self.gripper_state = JointState()
-        self.robot_state = JointState()
+        # self.gripper_state = JointState()
+        # self.robot_state = JointState()
+        self.command_header = None
+        self.gripper_state = {
+            'position': np.zeros(2),
+            'velocity': np.zeros(2),
+            'acceleration': np.zeros(2)}
+        self.robot_state = {
+            'position': np.zeros(7),
+            'velocity': np.zeros(7),
+            'acceleration': np.zeros(7)}
         self.ee_goal_pos = None
         self.ee_goal_quat = None
+
+        #ROS Initialization
+        self.command_pub = rospy.Publisher(self.joint_command_topic, JointState, queue_size=1, tcp_nodelay=True, latch=False)
+        self.state_sub = rospy.Subscriber(self.joint_states_topic, JointState, self.robot_state_callback, queue_size=1)
+        self.ee_goal_sub = rospy.Subscriber(self.ee_goal_topic, PoseStamped, self.ee_goal_callback, queue_size=1)
+        self.control_freq = float(1.0/self.control_dt)
+        self.rate = rospy.Rate(self.control_freq)
 
         self.state_sub_on = False
         self.goal_sub_on = False
         self.tstep = 0
         self.start_t = None
+        self.first_iter = True
 
     def robot_state_callback(self, msg):
         self.state_sub_on = True
-
+        self.command_header = msg.header
         #save gripper state
-        self.gripper_state.header = msg.header
-        self.gripper_state.position = msg.position[0:2]
-        self.gripper_state.velocity = msg.velocity[0:2]
-        self.gripper_state.effort = msg.effort[0:2]
+        # self.gripper_state.header = msg.header
+        # self.gripper_state.position = msg.position[0:2]
+        # self.gripper_state.velocity = msg.velocity[0:2]
+        # self.gripper_state.effort = msg.effort[0:2]
+        self.gripper_state['position'] = np.array(msg.position[0:2])
+        self.gripper_state['velocity'] = np.array(msg.velocity[0:2])
+        # #save robot state
+        # self.robot_state.header = msg.header
+        # self.robot_state.position = msg.position[2:]
+        # self.robot_state.velocity = msg.velocity[2:]
+        # self.robot_state.effort = msg.effort[2:]
+        self.robot_state['position'] = np.array(msg.position[2:])
+        self.robot_state['velocity'] = np.array(msg.velocity[2:])
+        self.robot_state['acceleration'] = np.zeros_like(self.robot_state['velocity'])
 
-        #save robot state
-        self.robot_state.header = msg.header
-        self.robot_state.position = msg.position[2:]
-        self.robot_state.velocity = msg.velocity[2:]
-        self.robot_state.effort = msg.effort[2:]
 
     def ee_goal_callback(self, msg):
         self.goal_sub_on = True
@@ -98,16 +110,38 @@ class MPCReacherNode():
                     self.policy.update_params(goal_ee_pos = self.ee_goal_pos,
                         goal_ee_quat = self.ee_goal_quat)
                     self.new_ee_goal = False
-                    pass
 
+                #get mpc command
+                command = self.policy.get_command(
+                    self.tstep, self.robot_state, control_dt=self.control_dt, WAIT=True)
+
+                #publish mpc command
+                self.mpc_command.header = self.command_header
+                self.mpc_command.header.stamp = rospy.Time.now()
+                self.mpc_command.position = command['position']
+                self.mpc_command.velocity = command['velocity']
+                self.mpc_command.effort =  command['acceleration']
+                self.command_pub.publish(self.mpc_command)
+
+                #update tstep
                 if self.tstep == 0:
+                    rospy.loginfo('[MPCPoseReacher]: Controller running')
                     self.start_t = rospy.get_time()
                 self.tstep = rospy.get_time() - self.start_t
 
+            else:
+                if (not self.state_sub_on) and (self.first_iter):
+                    rospy.loginfo('[MPCPoseReacher]: Waiting for robot state.')
+                if (not self.goal_sub_on) and (self.first_iter):
+                    rospy.loginfo('[MPCPoseReacher]: Waiting for ee goal.')
+            
+            self.first_iter = False
             self.rate.sleep()
     
     def close(self):
-        pass
+        self.command_pub.unregister()
+        self.state_sub.unregister()
+        self.ee_goal_sub.unregister()
 
 
 if __name__ == "__main__":
